2c9223fb42826a0e3089f896302c1cd25633c361
Thaslim <ta@ysg.co.in>
2017-08-08 18:34:02 +0530

    all changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,55 @@
+
+--
+-- Database: app_user_role
+--
+
+--
+-- Dumping data for table application
+--
+
 INSERT IGNORE INTO application (id, name) VALUES
-('09f919cd-dac6-4bfc-a0f2-d6cf1c098af2', 'DCO');
+('bf66ef7f-1f37-40d8-b08c-b82d20f408f0', 'APP_ROLE');
+
+--
+-- Dumping data for table city
+--
+
+INSERT IGNORE INTO city (id, name) VALUES
+('2febe4cb-8b44-4cb9-b60c-1d053ec369f9', 'Bengaluru'),
+('3e2feb9c-0a6a-4706-aa7d-55949c486097', 'Chennai'),
+('48581a9c-0bcc-4081-bc80-0ae156bf2a7a', 'Guwahati');
+
+--
+-- Dumping data for table registered_user
+--
+
+INSERT IGNORE INTO registered_user (id, firstname, lastname, mobile, email, actv_ind) VALUES
+('f7ce13bf-14ef-4d66-852f-a07dff45090e', 'Thaslim', 'Abdul Azees', '9600822990', 'ta@ysg.co.in', 'Y');
+
+--
+-- Dumping data for table role
+--
+
+INSERT IGNORE INTO role (id, name) VALUES
+('COMP_HEAD', 'Compliance Head'),
+('FIN_HEAD', 'Finance Head'),
+('SUPER_ADMIN', 'SUPER_ADMIN');
+
+--
+-- Dumping data for table user_city
+--
+
+INSERT IGNORE INTO user_city (user_id, city_id) VALUES
+('f7ce13bf-14ef-4d66-852f-a07dff45090e', '2febe4cb-8b44-4cb9-b60c-1d053ec369f9');
+
+--
+-- Dumping data for table user_role
+--
+
+INSERT IGNORE INTO user_role (user_id, role_id, app_id) VALUES
+('f7ce13bf-14ef-4d66-852f-a07dff45090e', 'SUPER_ADMIN', 'bf66ef7f-1f37-40d8-b08c-b82d20f408f0');
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,89 +1,89 @@
 --
--- Database: `app_user_role`
+-- Database: app_user_role
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `application`
+-- Table structure for table application
 --
 
-CREATE TABLE IF NOT EXISTS `application` (
-  `id` char(36) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+CREATE TABLE IF NOT EXISTS application (
+  id char(36) NOT NULL,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `city`
+-- Table structure for table city
 --
 
-CREATE TABLE IF NOT EXISTS `city` (
-  `id` char(36) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+CREATE TABLE IF NOT EXISTS city (
+  id char(36) NOT NULL,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `registered_user`
+-- Table structure for table registered_user
 --
 
-CREATE TABLE IF NOT EXISTS `registered_user` (
-  `id` char(36) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
-  `mobile` varchar(10) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
-  `actv_ind` enum('Y','N') NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_email` (`email`)
+CREATE TABLE IF NOT EXISTS registered_user (
+  id char(36) NOT NULL,
+  firstname varchar(100) NOT NULL,
+  lastname varchar(100) NOT NULL,
+  mobile varchar(10) DEFAULT NULL,
+  email varchar(100) NOT NULL,
+  actv_ind enum('Y','N') NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY user_email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `role`
+-- Table structure for table role
 --
 
-CREATE TABLE IF NOT EXISTS `role` (
-  `id` char(36) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `role_name` (`name`)
+CREATE TABLE IF NOT EXISTS role (
+  id char(36) NOT NULL,
+  name varchar(100) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY role_name (name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_city`
+-- Table structure for table user_city
 --
 
-CREATE TABLE IF NOT EXISTS `user_city` (
-  `user_id` char(36) NOT NULL,
-  `city_id` char(36) NOT NULL,
-  PRIMARY KEY (`user_id`,`city_id`),
-  KEY `city_id` (`city_id`)
+CREATE TABLE IF NOT EXISTS user_city (
+  user_id char(36) NOT NULL,
+  city_id char(36) NOT NULL,
+  PRIMARY KEY (user_id,city_id),
+  KEY city_id (city_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_role`
+-- Table structure for table user_role
 --
 
-CREATE TABLE IF NOT EXISTS `user_role` (
-  `user_id` char(36) NOT NULL,
-  `role_id` char(36) NOT NULL,
-  `app_id` char(36) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`,`app_id`),
-  KEY `role_id` (`role_id`),
-  KEY `app_id` (`app_id`)
+CREATE TABLE IF NOT EXISTS user_role (
+  user_id char(36) NOT NULL,
+  role_id char(36) NOT NULL,
+  app_id char(36) NOT NULL,
+  PRIMARY KEY (user_id,role_id,app_id),
+  KEY role_id (role_id),
+  KEY app_id (app_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ----
@@ -91,18 +91,17 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 ----
 --
 ----
----- Constraints for table `user_city`
-----
---ALTER TABLE `user_city`
---  ADD CONSTRAINT `user_city_ibfk_1` FOREIGN KEY (`city_id`) REFERENCES `CITY` (`id`),
---  ADD CONSTRAINT `user_city_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `registered_user` (`id`);
+-- Constraints for table user_city
 --
-----
----- Constraints for table `user_role`
-----
---ALTER TABLE `user_role`
---  ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `registered_user` (`id`),
---  ADD CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
---  ADD CONSTRAINT `user_role_ibfk_3` FOREIGN KEY (`app_id`) REFERENCES `application` (`id`);
---COMMIT;
+ALTER TABLE user_city ADD CONSTRAINT FOREIGN KEY (city_id) REFERENCES city (id);
+ALTER TABLE user_city ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES registered_user (id);
+
+--
+-- Constraints for table user_role
+--
+ALTER TABLE user_role
+  ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES registered_user (id),
+  ADD CONSTRAINT FOREIGN KEY (role_id) REFERENCES role (id),
+  ADD CONSTRAINT FOREIGN KEY (app_id) REFERENCES application (id);
+COMMIT;
 
